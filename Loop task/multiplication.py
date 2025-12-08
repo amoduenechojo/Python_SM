f2851eaa1e20daa351977d9bd5db4eea51cb55d9
amoduenechojo <amoduchojo27@gmail.com>
2025-12-08 05:30:32 +0100

    <<loop task>>

---
+++ Loop task/multiplication.py
@@ -0,0 +1,2 @@
+digit = int(input("enter a digit))
+for i in range (1, 11)
---
+++ Loop task/sumofmultiples.py
@@ -0,0 +1,6 @@
+total = 0
+for i in range (1, 20001):
+    if i % 10 == 0:
+            total += 1
+            print ("sum: ", total)  
+
